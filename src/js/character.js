e9c0994ae0f9e684fecd85d80b9cfd722b8abd5d
Anna <k_anna_i@mail.ru>
2021-08-22 18:19:53 +0300

    220821

--- src/js/character.js
+++ src/js/character.js
@@ -1,14 +1,11 @@
 export default class Character {
-  constructor(name, type, health, level, attack, defend) {
-    if (name.length > 2 && name.length < 11 && typeof name == 'string') {
+  constructor(name) {
+    if (name.length > 2 && name.length < 11 && typeof name === 'string') {
       this.name = name;
     } else {
       throw new Error('Unimplemented');
     }
-    this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = attack;
-    this.defend = defend;
   }
 }
